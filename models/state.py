4b9280e4f268bf640dd5fafdf1cd88c22f853fdf
Babatunde <babatundeojomu2016@gmail.com>
2024-05-23 05:17:52 +0100

    State

--- models/state.py
+++ models/state.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ holds class State"""
 import models
@@ -32,3 +33,4 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
